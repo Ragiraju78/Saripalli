66367290f3754325ddb4530b58452fc63508a14f
Raju <ragiraju78@gmail.com>
2020-10-20 00:35:51 +0200

    Second commit

--- src/test/java/SanthoshTraders.java
+++ src/test/java/SanthoshTraders.java
@@ -1,5 +1,6 @@
 public class SanthoshTraders {
     public static void main(String[] args) {
         System.out.println("Santhosh Traders");
+        System.out.println("Saripalli, Narsapuram");
     }
 }
